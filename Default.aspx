a883b90c236750b4df0d3baa9ed52786b7e144c2
Ren <nx_quantum@hotmail.com>
2013-10-21 11:44:56 -0700

    Init'd repo

---
+++ Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="jCryptionNET.Default" ValidateRequest="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <input type="hidden" id="conType" runat="server" />
+        <input type="hidden" id="encryptedFileData" runat="server" />
+        <input type="hidden" id="name" runat="server" />
+        <asp:Button ID="btnSubmit" runat="server" Text="Send" />
+    </form>
+    <p id="status"><span style="font-size: 16px;">Encrypting channel ...</span></p>
+    String:
+        <input type="text" id="text" disabled="disabled" />
+    <button id="encrypt" disabled="disabled">encrypt</button>
+    <button id="decrypt" disabled="disabled">decrypt</button>
+    <button id="serverChallenge" disabled="disabled">get encrypted time from server</button><br />
+
+    <form enctype="multipart/form-data">
+        <input type="file" id="files" name="files[]" multiple /><br />
+    </form>
+    Log:<br />
+
+    <textarea cols="60" rows="25" id="log"></textarea>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+    <script type="text/javascript" src="/js/jquery-ajax-helpers.js"></script>
+    <script type="text/javascript" src="/js/jquery.jcryption.3.0.js"></script>
+    <script type="text/javascript" src="/js/lib-typedarrays.js"></script>
+    <script type="text/javascript" src="/js/main.js"></script>
+</body>
+</html>
---
+++ js/jquery-ajax-helpers.js
@@ -0,0 +1,112 @@
+﻿//methodName: should be in form PageName.aspx/MethodName
+//parms: should be in form "{'parm1':'value1', 'parm2':'value2'}"
+function jQueryCallWrapper(methodName, parms, successFunc, errorFunc, successFuncParm) {
+    var parmString = formatParmsForPost(parms);
+    $.ajax({
+        type: "POST",
+        url: methodName,
+        data: parmString,
+        contentType: "application/json; charset=utf-8",
+        success: function(msg) { successFunc(msg, successFuncParm); },
+        error: function(msg) { errorFunc(msg); }
+        /*,
+        dataFilter: function(data) {
+        //jQuery 1.4 now has a jQuery.parseJSON method that does the same, so this can be switched
+        //at any point.
+            if (typeof (JSON) !== 'undefined' && typeof (JSON.parse) === 'function') {
+                return JSON.parse(data);
+            }
+            else {
+                return eval('(' + data + ')');
+            }
+        }*/
+    });
+}
+//methodName: should be in form PageName.aspx/MethodName
+//parms: should be in form parmName=%22input%22 (%22 = quotation marks)
+//25/07/2011: simplified this function according to: http://encosia.com/save-yourself-some-typing-when-you-call-asp-net-services/
+function jQueryCallWrapperREST(methodName, parms, successFunc, errorFunc, successFuncParm) {
+    var parmString = formatParmsForQueryString(parms);
+    $.ajax({
+        type: "GET",
+        url: methodName + "?" + parmString,
+        contentType: "application/json; charset=utf-8",
+        success: function(response) { successFunc(response, successFuncParm); },
+        error: function(msg) { errorFunc(msg); }
+        /*,
+        dataFilter: function(data, type) {
+            //jQuery 1.4 now has a jQuery.parseJSON method that does the same, so this can be switched
+            //at any point.
+            var prsd;
+            if (typeof (JSON) !== 'undefined' && typeof (JSON.parse) === 'function') {
+                prsd = JSON.parse(data);
+                return prsd;
+            }
+            else {
+                prsd = jQuery.parseJSON(data); //eval('(' + data + ')');
+                return prsd;
+            }
+        }*/
+    });
+}
+
+function formatParmsForPost(parms) {
+    var output;
+    if (parms instanceof Array) {
+        output = formatSimpleParmsForPost(parms);
+    }
+    else if (parms instanceof Object) {
+        if (typeof (JSON) !== 'undefined' && typeof (JSON.stringify) === 'function') {
+            output = JSON.stringify(parms);
+        }
+        else {
+            alert("JSON parser not present");
+        }
+
+    }
+    return output;
+}
+//expects array of strings in format parmName=input
+function formatSimpleParmsForPost(parms){
+    var out = "{", tempArr = null, i = 0;
+
+    for (i = 0; i < parms.length; i++) {
+        tempArr = parms[i].split('=');
+        out += "'" + tempArr[0] + "':'" + tempArr[1].replace(/'/g, "\\'").replace(/"/g, "\\\"") + "'" + (i == (parms.length - 1) ? '' : ',');
+    }
+    return out + "}";
+}
+//expects array of strings in format parmName=input
+function formatParmsForQueryString(parms) {
+    var out = "", tempArr = null, i = 0;
+    for (i = 0; i < parms.length; i++) {
+        tempArr = parms[i].split('=');
+        out += tempArr[0] + '=%22' + tempArr[1] + '%22' + (i == (parms.length - 1) ? '' : '&');
+    }
+    return out;
+}
+//Use when transferring complex object types.
+//DTO (Data Transfer Object): Used to encapsulate Web Method parameters. Ex. DTO['parmName1'] = parmObj1; DTO['parmName2'] = parmObj2; etc.
+//At this point, because the method below uses JSON.stringify, the page must reference "/jquery/json2.js".
+function jQueryCallWrapperDTO(methodName, DTO, successFunc, errorFunc, successFuncParm) {
+    var s=JSON.stringify(DTO);
+    $.ajax({
+        type: "POST",
+        url: methodName,
+        data: s,
+        contentType: "application/json; charset=utf-8",
+        success: function(msg) { successFunc(msg, successFuncParm); },
+        error: function(msg) { errorFunc(msg); }
+        /*,
+        dataFilter: function(data) {
+        //jQuery 1.4 now has a jQuery.parseJSON method that does the same, so this can be switched
+        //at any point.
+            if (typeof (JSON) !== 'undefined' && typeof (JSON.parse) === 'function') {
+                return JSON.parse(data);
+            }
+            else {
+                return eval('(' + data + ')');
+            }
+        }*/
+    });
+}
