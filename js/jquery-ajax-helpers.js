cc206a797b7cf9073596e410d7537918a5be8af1
Ren Ramkhelawan <triniMahn@users.noreply.github.com>
2013-10-21 22:04:13 -0700

    Update jquery-ajax-helpers.js

--- js/jquery-ajax-helpers.js
+++ js/jquery-ajax-helpers.js
@@ -7,7 +7,7 @@
 * http://www.opensource.org/licenses/mit-license.php
 *
 * If you need any further information about this script please
-* visit my homepage or contact me under ren@arkitekt.ca
+* visit my homepage or contact me at ren@arkitekt.ca
 */
 
 //methodName: should be in form PageName.aspx/MethodName
